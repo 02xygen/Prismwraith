130ff9b8f1cc05fbcbcb526dad41601c7cd9cc75
02xygen <104470575+02xygen@users.noreply.github.com>
2023-02-02 21:42:49 -0800

    Fixed some animations and broke others

--- Assets/Scripts/Gun_Controller.cs
+++ Assets/Scripts/Gun_Controller.cs
@@ -8,15 +8,15 @@ public class Gun_Controller : MonoBehaviour
     public Animator gunAnim;
     public void OnShoot(InputAction.CallbackContext context)
     {
-        gunAnim.SetTrigger("Shoot");
+  
     }
     public void OnSiphon(InputAction.CallbackContext context)
     {
-        gunAnim.SetTrigger("Siphon");
+
     }
     public void OnEject(InputAction.CallbackContext context)
     {
-        gunAnim.SetTrigger("Eject");
+    
     }
 
 }
